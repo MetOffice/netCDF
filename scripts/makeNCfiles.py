f2935ccea15bbd43f83aacd67d672cce22ccae0c
marqh <mark.hedley@metoffice.gov.uk>
2021-11-04 11:02:34 +0000

    stage and improver

--- scripts/makeNCfiles.py
+++ scripts/makeNCfiles.py
@@ -5,22 +5,32 @@ import subprocess
 import unittest
 
 class TestNetCDFBuild(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestNetCDFBuild, self).__init__(*args, **kwargs)
+        self.files = []
+
+
     def _build_file(self, infile):
         outfile = infile.replace('.cdl', '.nc')
         acall = ['ncgen', '-4', '-o', outfile, infile]
-        #print(' '.join(acall))
-        #subprocess.check_output(acall, stderr=subprocess.STDOUT)
         try:
             subprocess.check_output(acall, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             raise ValueError(' '.join(acall) + '\n{}'.format(e.output))
+        finally:
+            if os.path.exists(outfile):
+                self.files.append(outfile)
+
+    def tearDown(self):
+        for outfile in self.files:
+            if os.path.exists(outfile):
+                os.remove(outfile)
 
 
 for f in glob.glob('**/*.cdl', recursive=True):
     def make_a_test(inf):
         infile = copy.copy(inf)
         outfile = f.replace('.cdl', '.nc')
-        #print(outfile)
         def test_ncgen(self):
             self._build_file(infile)
             self.assertTrue(os.path.exists(outfile),
